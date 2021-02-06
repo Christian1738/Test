2b36d309f029172341218c6966a8a0bb4d61dfae
Christian1738 <78243633+Christian1738@users.noreply.github.com>
2021-02-05 22:23:49 -0500

    Add files via upload

---
+++ untitled.js
@@ -0,0 +1,14 @@
+var name = 'Christian'
+let age = 15
+const gender = 'male' 
+let grade = 9;
+let height = 5,9;
+let weight = 155;
+const bday = 3/12;
+let todayismonday = {false;}
+const Christian = {name:Christian , favoritefood:'dominos'}
+let favoritecars = ['Ford 42 roadster', 'Rolls-Royce Dawn Drophead', 'Lamborghini Miura']
+let application1 = {
+	Name: 'Kris',
+	Age: 384
+};
